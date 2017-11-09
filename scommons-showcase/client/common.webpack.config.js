37da41e6b8362ef9c1289bc24ab2bb0ec1ee41e5
Viktor Podzigun <viktor.podzigun@gmail.com>
2017-11-09 08:27:21 +0100

    Switched to url-loader for using inline css images

--- scommons-showcase/client/common.webpack.config.js
+++ scommons-showcase/client/common.webpack.config.js
@@ -5,8 +5,17 @@ module.exports = {
 //  target: 'web',
 
   module: {
+//    loaders: [{
+//      loader: "file-loader?name=[hash].[ext]&outputPath=styles/&publicPath=../",
+//      test: /\.(ico|png|gif|jpe?g|svg)$/i,
+//      exclude: /node_modules/
+//    }, {
+//      loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&localIdentName=[local]__[hash:base64:5]&sourceMap!resolve-url-loader?sourceMap'),
+//      test: /\.css$/,
+//      exclude: /node_modules/
+//    }]
     loaders: [{
-      loader: "file-loader?name=[hash].[ext]&outputPath=styles/&publicPath=../",
+      loader: "url-loader",
       test: /\.(ico|png|gif|jpe?g|svg)$/i,
       exclude: /node_modules/
     }, {
