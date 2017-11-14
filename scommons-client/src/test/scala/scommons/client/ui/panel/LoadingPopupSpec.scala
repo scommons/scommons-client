16a5bb74d79b1b00a90ce0f04e13bc2b4a16d685
Viktor Podzigun <viktor.podzigun@gmail.com>
2017-11-14 08:56:11 +0100

    Added rendering props tests for StatusPopup and LoadingPopup components

--- scommons-client/src/test/scala/scommons/client/ui/panel/LoadingPopupSpec.scala
+++ scommons-client/src/test/scala/scommons/client/ui/panel/LoadingPopupSpec.scala
@@ -4,13 +4,32 @@ import org.scalatest.{FlatSpec, Matchers}
 import scommons.client.test.TestUtils._
 import scommons.client.test.TestVirtualDOM._
 import scommons.client.test.raw.ReactTestUtils._
+import scommons.client.test.raw.ShallowRenderer
 import scommons.client.test.raw.TestReactDOM._
 import scommons.client.ui.panel.PopupPanelCss._
 import scommons.react.modal.NativeReactModal
 
 class LoadingPopupSpec extends FlatSpec with Matchers {
 
-  "rendering" should "render the component" in {
+  "rendering" should "pass correct props to the children" in {
+    //given
+    val component = E(LoadingPopup())(A.wrapped := LoadingPopupProps(show = true))()
+
+    //when
+    val result = ShallowRenderer.renderAndGetOutput(component)
+
+    //then
+    result.`type` shouldBe NativeReactModal
+    result.props.isOpen shouldBe true
+    result.props.shouldCloseOnOverlayClick shouldBe false
+    result.props.overlayClassName shouldBe loadingOverlay
+    result.props.className shouldBe loadingContent
+    result.props.children.`type` shouldBe "img"
+    result.props.children.props.className shouldBe loadingImg
+    result.props.children.props.src shouldBe ""
+  }
+
+  it should "render the component in the DOM" in {
     //given
     val component = E(LoadingPopup())(A.wrapped := LoadingPopupProps(show = true))()
 
--- scommons-client/src/test/scala/scommons/client/ui/panel/StatusPopupSpec.scala
+++ scommons-client/src/test/scala/scommons/client/ui/panel/StatusPopupSpec.scala
@@ -4,13 +4,33 @@ import org.scalatest.{FlatSpec, Matchers}
 import scommons.client.test.TestUtils._
 import scommons.client.test.TestVirtualDOM._
 import scommons.client.test.raw.ReactTestUtils._
+import scommons.client.test.raw.ShallowRenderer
 import scommons.client.test.raw.TestReactDOM._
 import scommons.client.ui.panel.PopupPanelCss._
 import scommons.react.modal.NativeReactModal
 
 class StatusPopupSpec extends FlatSpec with Matchers {
 
-  "rendering" should "render the component" in {
+  "rendering" should "pass correct props to the children" in {
+    //given
+    val props = StatusPopupProps("test text", show = true, () => ())
+    val component = E(StatusPopup())(A.wrapped := props)()
+
+    //when
+    val result = ShallowRenderer.renderAndGetOutput(component)
+
+    //then
+    result.`type` shouldBe NativeReactModal
+    result.props.isOpen shouldBe true
+    result.props.shouldCloseOnOverlayClick shouldBe true
+    result.props.overlayClassName shouldBe "scommons-status-no-overlay"
+    result.props.className shouldBe statusContent
+    result.props.children.`type` shouldBe WithAutoHide()
+    result.props.children.props.wrapped shouldBe WithAutoHideProps(props.onHide)
+    result.props.children.props.children shouldBe props.text
+  }
+
+  it should "render the component in the DOM" in {
     //given
     val text = "test text"
     val component = E(StatusPopup())(A.wrapped := StatusPopupProps(text, show = true, () => ()))()
