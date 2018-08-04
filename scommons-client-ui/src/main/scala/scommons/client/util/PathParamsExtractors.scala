af739f7bf2dff914c72d47659e27a80f4cb1d155
Viktor Podzigun <viktor.podzigun@gmail.com>
2018-08-04 13:20:14 +0200

    Added exact param to PathParamsExtractors.extractId

--- scommons-client-ui/src/main/scala/scommons/client/util/PathParamsExtractors.scala
+++ scommons-client-ui/src/main/scala/scommons/client/util/PathParamsExtractors.scala
@@ -4,11 +4,16 @@ import scala.util.matching.Regex
 
 trait PathParamsExtractors {
 
-  def extractId(idRegex: Regex, path: String): Option[Int] = {
-    for {
-      idRegex(id) <- idRegex.findPrefixMatchOf(path)
-    } yield {
-      id.toInt
+  def extractId(idRegex: Regex, path: String, exact: Boolean = false): Option[Int] = {
+    if (exact && !idRegex.pattern.matcher(path).matches()) {
+      None
+    }
+    else {
+      for {
+        idRegex(id) <- idRegex.findPrefixMatchOf(path)
+      } yield {
+        id.toInt
+      }
     }
   }
 }
--- scommons-client-ui/src/test/scala/scommons/client/util/PathParamsExtractorsSpec.scala
+++ scommons-client-ui/src/test/scala/scommons/client/util/PathParamsExtractorsSpec.scala
@@ -18,9 +18,26 @@ class PathParamsExtractorsSpec extends TestSpec {
     extractId(idRegex, s"$prefix/test") shouldBe None
     extractId(idRegex, s"$prefix/1") shouldBe Some(1)
     extractId(idRegex, s"$prefix/123") shouldBe Some(123)
-    extractId(idRegex, s"$prefix/123") shouldBe Some(123)
     extractId(idRegex, s"$prefix/123?test") shouldBe Some(123)
     extractId(idRegex, s"$prefix/123/test") shouldBe Some(123)
     extractId(idRegex, s"$prefix/123$prefix/456") shouldBe Some(123)
+  }
+  
+  it should "extract id only if exact match when extractId" in {
+    //given
+    val prefix = "/some-path"
+    val idRegex = s"$prefix/(\\d+)".r
+
+    //when & then
+    extractId(idRegex, "", exact = true) shouldBe None
+    extractId(idRegex, "/", exact = true) shouldBe None
+    extractId(idRegex, "/test", exact = true) shouldBe None
+    extractId(idRegex, s"/test/$prefix/1", exact = true) shouldBe None
+    extractId(idRegex, s"$prefix/test", exact = true) shouldBe None
+    extractId(idRegex, s"$prefix/1", exact = true) shouldBe Some(1)
+    extractId(idRegex, s"$prefix/123", exact = true) shouldBe Some(123)
+    extractId(idRegex, s"$prefix/123?test", exact = true) shouldBe None
+    extractId(idRegex, s"$prefix/123/test", exact = true) shouldBe None
+    extractId(idRegex, s"$prefix/123$prefix/456", exact = true) shouldBe None
   }
 }
