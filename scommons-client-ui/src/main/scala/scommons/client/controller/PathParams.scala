158a51d3d6dc8402d2104f72bcb1dd388319f3b2
Viktor Podzigun <viktor.podzigun@gmail.com>
2018-11-12 11:01:44 +0100

    Added generic PathParams.extract method

--- scommons-client-ui/src/main/scala/scommons/client/controller/PathParams.scala
+++ scommons-client-ui/src/main/scala/scommons/client/controller/PathParams.scala
@@ -4,15 +4,16 @@ import scala.util.matching.Regex
 
 class PathParams private(val path: String) extends AnyVal {
 
-  def extractInt(regex: Regex, exact: Boolean = false): Option[Int] = {
-    if (exact && !regex.pattern.matcher(path).matches()) {
-      None
-    }
+  def extractInt(regex: Regex, exact: Boolean = false): Option[Int] =
+    extract(regex, exact).map(_.toInt)
+  
+  def extract(regex: Regex, exact: Boolean = false): Option[String] = {
+    if (exact && !regex.pattern.matcher(path).matches()) None
     else {
       for {
         regex(id) <- regex.findPrefixMatchOf(path)
       } yield {
-        id.toInt
+        id
       }
     }
   }
