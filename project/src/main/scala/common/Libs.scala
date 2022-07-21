69e24bc02b3aa0283147e7f09d6122c0f76116ca
Viktor Podzigun <viktor.podzigun@gmail.com>
2022-07-21 22:05:44 +0200

    Prepare for next release

--- project/src/main/scala/common/Libs.scala
+++ project/src/main/scala/common/Libs.scala
@@ -6,10 +6,10 @@ import scommons.sbtplugin.project.CommonLibs
 
 object Libs extends CommonLibs {
 
-  val scommonsNodejsVersion = "1.0.0-SNAPSHOT"
-  val scommonsReactVersion = "1.0.0-SNAPSHOT"
-  private val scommonsApiVersion = "1.0.0-SNAPSHOT"
-  private val sjsReactJsVer = "1.0.0-SNAPSHOT"
+  val scommonsNodejsVersion = "0.9.0"
+  val scommonsReactVersion = "0.9.0"
+  private val scommonsApiVersion = "0.9.0"
+  private val sjsReactJsVer = "1.0.0"
 
   lazy val scommonsApiXhr = Def.setting("org.scommons.api" %%% "scommons-api-xhr" % scommonsApiVersion)
   
